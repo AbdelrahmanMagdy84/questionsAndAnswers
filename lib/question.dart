2c098e15e5da744f1ad105d7005f57701e1fe0a7
AbdelrahmanMagdy84 <boodyboo84@gmail.com>
2021-12-28 09:31:41 -0800

    question second constructor

--- lib/question.dart
+++ lib/question.dart
@@ -4,6 +4,7 @@ class Question extends StatelessWidget {
   final String _question;
   final int _questionNumber;
   Question(this._question, this._questionNumber);
+  Question.second(this._questionNumber, this._question);
   @override
   Widget build(BuildContext context) {
     return Container(
